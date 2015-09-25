c07a7f1c468cb1b9146150c59e9a1509d3663eeb
mikedutuandu <mikedutuandu@gmail.com>
2015-09-25 23:23:41 +0700

    function get handbook and get section by handbook

--- Organisation/Handbook/Handbook.php
+++ Organisation/Handbook/Handbook.php
@@ -1,4 +1,5 @@
 <?php
+
 // src/AppBundle/Entity/Organisation/Handbook/Handbook.php
 
 namespace AppBundle\Entity\Organisation\Handbook;
@@ -6,22 +7,47 @@ namespace AppBundle\Entity\Organisation\Handbook;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
-
+use JMS\Serializer\Annotation as Serializer;
+use Hateoas\Configuration\Annotation as Hateoas;
 
 /**
+ * @Serializer\XmlRoot("handbook")
+ * @Hateoas\Relation("self", href = @Hateoas\Route(
+ *         "get_handbook",
+ *         parameters = { "handbook" = "expr(object.getId())" },
+ *         absolute = true
+ *     )
+ * )
+ * @Hateoas\Relation(
+ *  "sestions",
+ *  href= @Hateoas\Route(
+ *         "get_handbook_sections",
+ *         parameters = { "handbookId" = "expr(object.getId())"},
+ *         absolute = true
+ *     ),
+ *  exclusion=@Hateoas\Exclusion(excludeIf="expr(object.getSections().count() == 0)")
+ * )
+ * @Hateoas\Relation(
+ *  "organisation",
+ *  href= @Hateoas\Route(
+ *         "get_organisation",
+ *         parameters = { "organisation" = "expr(object.getOrganisation().getId())"},
+ *         absolute = true
+ *     ),
+ * )
+ * 
  * @ORM\Entity
  * @ORM\Table(name="handbook")
  * @Gedmo\Loggable()
  */
-class Handbook
-{
+class Handbook {
+
     /**
      * @ORM\Id
      * @ORM\Column(type="integer",options={"unsigned":true})
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
-
 
     /**
      * @var string
@@ -33,8 +59,7 @@ class Handbook
     /**
      * @param string $version
      */
-    public function setVersion($version)
-    {
+    public function setVersion($version) {
         foreach ($this->sections as $section) {
 //            $section = new Section();
             $section->setVersion($version);
@@ -42,21 +67,21 @@ class Handbook
         $this->version = $version;
     }
 
-
     /**
      * @ORM\OneToOne(targetEntity="AppBundle\Entity\Organisation\Organisation", inversedBy="handbook")
      * @ORM\JoinColumn(name="id_organisation", referencedColumnName="id")
-     **/
+     * @Serializer\Exclude
+     * */
     private $organisation;
 
     /**
      * @var ArrayCollection
      * @ORM\OneToMany(targetEntity="AppBundle\Entity\Organisation\Handbook\Section", orphanRemoval=true, mappedBy="handbook", cascade={"persist", "remove", "merge"})
-     **/
+     * @Serializer\Exclude
+     * */
     private $sections;
 
-    public function addSection(Section $section)
-    {
+    public function addSection(Section $section) {
         $this->sections->add($section);
         $section->setHandbook($this);
     }
@@ -64,8 +89,7 @@ class Handbook
     /**
      * @param Section $section
      */
-    public function removeChild(Section $section)
-    {
+    public function removeChild(Section $section) {
         $this->children->removeElement($section);
         $section->setHandbook(null);
     }
@@ -92,104 +116,91 @@ class Handbook
     /**
      * @return string
      */
-    public function getTitle()
-    {
+    public function getTitle() {
         return $this->title;
     }
 
     /**
      * @param string $title
      */
-    public function setTitle($title)
-    {
+    public function setTitle($title) {
         $this->title = $title;
     }
 
     /**
      * @return int
      */
-    public function getYear()
-    {
+    public function getYear() {
         return $this->year;
     }
 
     /**
      * @param int $year
      */
-    public function setYear($year)
-    {
+    public function setYear($year) {
         $this->year = $year;
     }
 
     /**
      * @return string
      */
-    public function getDescription()
-    {
+    public function getDescription() {
         return $this->description;
     }
 
     /**
      * @param string $description
      */
-    public function setDescription($description)
-    {
+    public function setDescription($description) {
         $this->description = $description;
     }
 
     /**
      * @return mixed
      */
-    public function getOrganisation()
-    {
+    public function getOrganisation() {
         return $this->organisation;
     }
 
     /**
      * @param mixed $organisation
      */
-    public function setOrganisation($organisation)
-    {
+    public function setOrganisation($organisation) {
         $this->organisation = $organisation;
     }
 
     /**
      * @return mixed
      */
-    public function getId()
-    {
+    public function getId() {
         return $this->id;
     }
 
     /**
      * @param mixed $id
      */
-    public function setId($id)
-    {
+    public function setId($id) {
         $this->id = $id;
     }
 
     /**
      * @return ArrayCollection
      */
-    public function getSections()
-    {
+    public function getSections() {
         return $this->sections;
     }
 
     /**
      * @param ArrayCollection $sections
      */
-    public function setSections($sections)
-    {
+    public function setSections($sections) {
         $this->sections = $sections;
     }
 
     /**
      * @return string
      */
-    public function getVersion()
-    {
+    public function getVersion() {
         return $this->version;
     }
 
