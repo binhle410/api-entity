0fa9cd6961e7e134da45f57cea2172a8ea3cb09c
acquykoten <acquykoten@gmail.com>
2015-11-03 15:44:31 +0700

    location full

--- Core/Location/District.php
+++ Core/Location/District.php
@@ -17,7 +17,6 @@ class District
     private $id;
 
 
-
     /**
      * @var ArrayCollection Wards
      * @ORM\OneToMany(targetEntity="AppBundle\Entity\Core\Location\Ward", mappedBy="districts")
@@ -26,7 +25,7 @@ class District
 
     /**
      * @var string
-     * ORM\Column(length=50, type="string")
+     * @ORM\Column(length=50, type="string")
      */
     private $name;
 
@@ -36,4 +35,70 @@ class District
      * @ORM\JoinColumn(name="id_city", referencedColumnName="id", onDelete="CASCADE")
      **/
     private $city;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getWards()
+    {
+        return $this->wards;
+    }
+
+    /**
+     * @param ArrayCollection $wards
+     */
+    public function setWards($wards)
+    {
+        $this->wards = $wards;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return City
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param City $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+
 }
--- Core/Location/Ward.php
+++ Core/Location/Ward.php
@@ -25,7 +25,56 @@ class Ward
 
     /**
      * @var string
-     * ORM\Column(length=50, type="string")
+     * @ORM\Column(length=50, type="string")
      */
     private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return District
+     */
+    public function getDistrict()
+    {
+        return $this->district;
+    }
+
+    /**
+     * @param District $district
+     */
+    public function setDistrict($district)
+    {
+        $this->district = $district;
+    }
+
 }
