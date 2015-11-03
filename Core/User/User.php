92f14a61418dc5869551f720024dfa20a64ae899
Lê Thanh Bình <binh@sunrise.vn>
2015-11-03 19:41:43 +0700

    $position

--- Core/User/User.php
+++ Core/User/User.php
@@ -53,7 +53,7 @@ class User extends BaseUser
         $this->dateAdded = new \DateTime('now');
         $this->birthday = new \Datetime();
         $this->tags = new ArrayCollection();
-        $this->grous = new ArrayCollection();
+        $this->groups = new ArrayCollection();
 
     }
 
