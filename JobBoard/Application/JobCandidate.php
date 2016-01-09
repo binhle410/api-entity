d2aae9c33746cf4b374d132cc0c9cde7f1765e58
mikedutuandu <mikedutuandu@gmail.com>
2016-01-09 10:35:40 +0700

    job cadidate crud

--- JobBoard/Application/JobCandidate.php
+++ JobBoard/Application/JobCandidate.php
@@ -1,17 +1,12 @@
 <?php
+
 namespace AppBundle\Entity\JobBoard\Application;
 
-use AppBundle\Entity\Accounting\Payroll\Salary;
-use AppBundle\Entity\Core\Location\Location;
-use AppBundle\Entity\Core\Tag;
 use AppBundle\Entity\Core\User\User;
 use AppBundle\Entity\JobBoard\Listing\JobListing;
-use AppBundle\Entity\Organisation\Organisation;
 use AppBundle\Services\Core\Framework\BaseVoterSupportInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
-
-
 use JMS\Serializer\Annotation as Serializer;
 use Hateoas\Configuration\Annotation as Hateoas;
 use Gedmo\Mapping\Annotation as Gedmo;
@@ -19,9 +14,28 @@ use Gedmo\Mapping\Annotation as Gedmo;
 /**
  * @ORM\Entity
  * @ORM\Table(name="job__application__candidate")
+ * 
+ * @Serializer\XmlRoot("jobcandidate")
+ * @Hateoas\Relation(
+ *  "self",
+ *  href= @Hateoas\Route(
+ *         "get_jobcandidates",
+ *         parameters = {},
+ *         absolute = true
+ *     ),
+ *  attributes = { "method" = {"put","delete"} },
+ * )
+ * @Hateoas\Relation(
+ *  "jobcandidate.post",
+ *  href= @Hateoas\Route(
+ *         "post_jobcandidate",
+ *         parameters = {},
+ *         absolute = true
+ *     )
+ * )
  */
-class JobCandidate implements BaseVoterSupportInterface
-{
+class JobCandidate implements BaseVoterSupportInterface {
+
     /**
      * @var int
      * @ORM\Id
@@ -30,8 +44,7 @@ class JobCandidate implements BaseVoterSupportInterface
      */
     private $id;
 
-    function __construct()
-    {
+    function __construct() {
         $this->observers = new ArrayCollection();
         $this->reviewers = new ArrayCollection();
         $this->folders = new ArrayCollection();
@@ -51,20 +64,18 @@ class JobCandidate implements BaseVoterSupportInterface
      */
     private $user;
 
-
     /**
      * @var ArrayCollection
      * @ORM\OneToMany(targetEntity="AppBundle\Entity\JobBoard\Application\CandidateInterview", mappedBy="candidate")
      */
     private $interviews;
-    public function addInterview(CandidateInterview $interviews)
-    {
+
+    public function addInterview(CandidateInterview $interviews) {
         $this->interviews->add($interviews);
         return $this;
     }
 
-    public function removeInterview(CandidateInterview $interviews)
-    {
+    public function removeInterview(CandidateInterview $interviews) {
         $this->interviews->removeElement($interviews);
         return $this;
     }
@@ -79,13 +90,11 @@ class JobCandidate implements BaseVoterSupportInterface
      */
     private $reviewers;
 
-    public function addReviewer(User $user)
-    {
+    public function addReviewer(User $user) {
         $this->reviewers->add($user);
     }
 
-    public function removeReviewer(User $user)
-    {
+    public function removeReviewer(User $user) {
         $this->reviewers->removeElement($user);
     }
 
@@ -99,13 +108,11 @@ class JobCandidate implements BaseVoterSupportInterface
      */
     private $observers;
 
-    public function addObserver(User $user)
-    {
+    public function addObserver(User $user) {
         $this->observers->add($user);
     }
 
-    public function removeObserver(User $user)
-    {
+    public function removeObserver(User $user) {
         $this->observers->removeElement($user);
     }
 
@@ -119,127 +126,110 @@ class JobCandidate implements BaseVoterSupportInterface
      */
     private $folders;
 
-    public function addFolder(CandidateFolder $folder)
-    {
+    public function addFolder(CandidateFolder $folder) {
         $this->folders->add($folder);
     }
 
-    public function removeFolder(CandidateFolder $folder)
-    {
+    public function removeFolder(CandidateFolder $folder) {
         $this->folders->removeElement($folder);
     }
 
     /**
      * @return int
      */
-    public function getId()
-    {
+    public function getId() {
         return $this->id;
     }
 
     /**
      * @param int $id
      */
-    public function setId($id)
-    {
+    public function setId($id) {
         $this->id = $id;
     }
 
     /**
      * @return JobListing
      */
-    public function getListing()
-    {
+    public function getListing() {
         return $this->listing;
     }
 
     /**
      * @param JobListing $listing
      */
-    public function setListing($listing)
-    {
+    public function setListing($listing) {
         $this->listing = $listing;
     }
 
     /**
      * @return JobListing
      */
-    public function getUser()
-    {
+    public function getUser() {
         return $this->user;
     }
 
     /**
      * @param JobListing $user
      */
-    public function setUser($user)
-    {
+    public function setUser($user) {
         $this->user = $user;
     }
 
     /**
      * @return ArrayCollection
      */
-    public function getInterviews()
-    {
+    public function getInterviews() {
         return $this->interviews;
     }
 
     /**
      * @param ArrayCollection $interviews
      */
-    public function setInterviews($interviews)
-    {
+    public function setInterviews($interviews) {
         $this->interviews = $interviews;
     }
 
     /**
      * @return ArrayCollection
      */
-    public function getReviewers()
-    {
+    public function getReviewers() {
         return $this->reviewers;
     }
 
     /**
      * @param ArrayCollection $reviewers
      */
-    public function setReviewers($reviewers)
-    {
+    public function setReviewers($reviewers) {
         $this->reviewers = $reviewers;
     }
 
     /**
      * @return ArrayCollection
      */
-    public function getObservers()
-    {
+    public function getObservers() {
         return $this->observers;
     }
 
     /**
      * @param ArrayCollection $observers
      */
-    public function setObservers($observers)
-    {
+    public function setObservers($observers) {
         $this->observers = $observers;
     }
 
     /**
      * @return ArrayCollection
      */
-    public function getFolders()
-    {
+    public function getFolders() {
         return $this->folders;
     }
 
     /**
      * @param ArrayCollection $folders
      */
-    public function setFolders($folders)
-    {
+    public function setFolders($folders) {
         $this->folders = $folders;
     }
-
 
 }
