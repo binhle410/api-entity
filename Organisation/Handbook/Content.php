26db75b9384818317e93459867cb7a1b80357588
Lê Thanh Bình <binh@sunrise.vn>
2016-04-06 10:03:10 +0700

    remove contents.post

--- Organisation/Handbook/Content.php
+++ Organisation/Handbook/Content.php
@@ -30,20 +30,6 @@ use Gedmo\Translatable\Translatable;
  *         absolute = true
  *     ),
  * )
- * @Hateoas\Relation("contents.post",
- *  href= @Hateoas\Route(
- *         "post_organisation_handbook_section_content",
- *        parameters = { "organisationId" = "expr(object.getSection().getHandbook().getOrganisation().getId())","handbookId" = "expr(object.getSection().getHandbook().getId())","sectionId"="expr(object.getSection().getId())"},
- *         absolute = true
- *     ),
- * )
- * @Hateoas\Relation("images.post",
- *  href= @Hateoas\Route(
- *         "post_organisation_handbook_section_content_image",
- *         parameters = { "organisationId" = "expr(object.getSection().getHandbook().getOrganisation().getId())","handbookId" = "expr(object.getSection().getHandbook().getId())","sectionId"="expr(object.getSection().getId())","content"="expr(object.getId())"},
- *         absolute = true
- *     ),
- * )
  * @Hateoas\Relation("image_url",
  *  href= @Hateoas\Route(
  *         "get_organisation_handbook_section_content_image_url",
