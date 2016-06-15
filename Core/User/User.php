f30a91188314c8710693bf70b969c92b6c1a4dc7
Lê Thanh Bình <binh@sunrise.vn>
2016-06-15 22:05:17 +0700

    - handbookretrievalcontroller -> cgetActions
    TODO:
    - users/id/positions: POST, PUT, DELETE
    - HATEOAS: JobCandidate to retrieve folders along with folder_reviewers
    - Map  CandidateFolder
    - JobListing Form CRUD - salary
    - QBSecurity Trait

--- Core/User/User.php
+++ Core/User/User.php
@@ -87,6 +87,7 @@ class User extends BaseUser implements BaseVoterSupportInterface, ListVoterSuppo
 {
     const CACHE_NS = 'system.user';
     /**
+     * @var int
      * @ORM\Id
      * @ORM\Column(type="integer",options={"unsigned":true})
      * @ORM\GeneratedValue(strategy="AUTO")
@@ -271,7 +272,7 @@ class User extends BaseUser implements BaseVoterSupportInterface, ListVoterSuppo
 
 
     /**
-     * @var array
+     *
      * @Serializer\Exclude()
      */
     protected $roles;
